430c79e953a2cb058a3a879669972cce72f4fb1e
gimminseog <gm27376@gmail.com>
2021-01-24 20:57:55 +0900

    백준 이분탐색

--- baek__13397.java
+++ baek__13397.java
@@ -1,66 +1,142 @@
+// import java.io.*;
+// import java.util.*;
+
+// class baek__13397 {
+
+// static int calc(int[] arr, int limit) {
+
+// int cnt = 0;
+// int min = arr[0];
+// int max = arr[0];
+
+// for (int i = 0; i < arr.length; i++) {
+
+// min = Math.min(min, arr[i]);
+// max = Math.max(max, arr[i]);
+
+// if (max - min > limit) {
+// cnt++;
+
+// min = arr[i];
+// max = arr[i];
+// }
+// }
+// cnt++;
+
+// return cnt;
+// }
+
+// public static void main(String[] args) throws IOException {
+// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+// String[] temp = br.readLine().split(" ");
+// int n = Integer.parseInt(temp[0]);
+// int m = Integer.parseInt(temp[1]);
+
+// int[] arr = new int[n];
+
+// temp = br.readLine().split(" ");
+// int min = -1;
+// int max = -1;
+
+// for (int i = 0; i < n; i++) {
+// arr[i] = Integer.parseInt(temp[i]);
+// if (min == -1 || min > arr[i])
+// min = arr[i];
+
+// if (max == -1 || max < arr[i]) {
+// max = arr[i];
+// }
+// }
+
+// int left = 0;
+// int right = max - min;
+// int ans = -1;
+
+// while (left <= right) {
+// int mid = (left + right) / 2;
+
+// if (calc(arr, mid) > m) {
+// left = mid + 1;
+// } else {
+// ans = mid;
+// right = mid - 1;
+// }
+// }
+
+// System.out.print(ans);
+
+// }
+// }
 import java.io.*;
 import java.util.*;
 
 class baek__13397 {
+    static int n;
+    static int m;
+    static int[] nums;
 
-    static int calc(int[] arr, int limit) {
+    static boolean check(int limit) {
+        int min = nums[0];
+        int max = nums[0];
 
-        int cnt = 0;
-        int min = arr[0];
-        int max = arr[0];
+        int cnt = 1;
 
-        for (int i = 0; i < arr.length; i++) {
-
-            min = Math.min(min, arr[i]);
-            max = Math.max(max, arr[i]);
-
-            if (max - min > limit) {
-                cnt++;
-
-                min = arr[i];
-                max = arr[i];
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < min) {
+                if (max - nums[i] > limit) {
+                    min = nums[i];
+                    max = nums[i];
+                    cnt++;
+                } else {
+                    min = nums[i];
+                }
+            } else if (nums[i] > max) {
+                if (nums[i] - min > limit) {
+                    max = nums[i];
+                    min = nums[i];
+                    cnt++;
+                } else {
+                    max = nums[i];
+                }
             }
         }
-        cnt++;
 
-        return cnt;
+        if (cnt > m)
+            return false;
+
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] temp = br.readLine().split(" ");
-        int n = Integer.parseInt(temp[0]);
-        int m = Integer.parseInt(temp[1]);
 
-        int[] arr = new int[n];
+        n = Integer.parseInt(temp[0]);
+        m = Integer.parseInt(temp[1]);
 
         temp = br.readLine().split(" ");
-        int min = -1;
-        int max = -1;
+        nums = new int[n];
+
+        int l = 0;
+        int r = 0;
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(temp[i]);
-            if (min == -1 || min > arr[i])
-                min = arr[i];
-
-            if (max == -1 || max < arr[i]) {
-                max = arr[i];
-            }
+            nums[i] = Integer.parseInt(temp[i]);
+            r = Math.max(nums[i], r);
         }
 
-        int left = 0;
-        int right = max - min;
-        int ans = -1;
+        int ans = r;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
+        while (l <= r) {
+            int mid = (l + r) / 2;
 
-            if (calc(arr, mid) > m) {
-                left = mid + 1;
+            if (check(mid)) {
+                ans = Math.min(ans, mid);
+                r = mid - 1;
             } else {
-                ans = mid;
-                right = mid - 1;
+                l = mid + 1;
             }
         }
 
--- baek__1981.java
+++ baek__1981.java
@@ -96,3 +96,106 @@
 // System.out.print(ans);
 // }
 // }
+import java.io.*;
+import java.util.*;
+
+class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class baek__1981 {
+
+    static int n;
+    static int[][] map;
+    static boolean[][] check;
+    static int[] dx = { -1, 0, 1, 0 };
+    static int[] dy = { 0, -1, 0, 1 };
+
+    static boolean bfs(int min, int max) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                check[i][j] = false;
+            }
+        }
+
+        Queue<Point> q = new LinkedList<>();
+
+        if (map[0][0] >= min && map[0][0] <= max) {
+            q.add(new Point(0, 0));
+            check[0][0] = true;
+        }
+
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+            int x = cur.x;
+            int y = cur.y;
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                    continue;
+                if (check[nx][ny])
+                    continue;
+                if (map[nx][ny] > max || map[nx][ny] < min)
+                    continue;
+                q.add(new Point(nx, ny));
+                check[nx][ny] = true;
+            }
+        }
+
+        if (check[n - 1][n - 1])
+            return true;
+        return false;
+    }
+
+    static boolean possible(int limit) {
+
+        for (int min = 0; min + limit <= 200; min++) {
+            if (bfs(min, min + limit))
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n][n];
+        check = new boolean[n][n];
+        String[] temp;
+
+        for (int i = 0; i < n; i++) {
+            temp = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(temp[j]);
+            }
+        }
+
+        int l = 0;
+        int r = 200;
+        int ans = 200;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (possible(mid)) {
+                r = mid - 1;
+                ans = Math.min(ans, mid);
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        System.out.print(ans);
+
+    }
+}
